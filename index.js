dfa474c6b226dd208a73af1e31f9e88874080580
silvazzy51-crypto <silvazzy51@gmail.com>
2026-05-18 15:14:09 -0300

    Initialize Discord bot client with login

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+const { Client, GatewayIntentBits } = require("discord.js");
 
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds]
+});
+
+client.once("ready", () => {
+  console.log(`Bot online: ${client.user.tag}`);
+});
+
+client.login(process.env.TOKEN);
