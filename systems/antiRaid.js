d3aa21d21c65f164755680e7596edf05c7eb9a72
silvazzy51-crypto <silvazzy51@gmail.com>
2026-05-18 15:29:46 -0300

    Implement anti-raid feature for guild members

--- systems/antiRaid.js
+++ systems/antiRaid.js
@@ -1,1 +1,39 @@
+const config = require("../config.json");
 
+const joins = new Map();
+let raid = new Map();
+
+module.exports = (client) => {
+  client.on("guildMemberAdd", async (member) => {
+    const guildId = member.guild.id;
+    const now = Date.now();
+
+    if (!joins.has(guildId)) joins.set(guildId, []);
+    if (!raid.has(guildId)) raid.set(guildId, false);
+
+    const list = joins.get(guildId);
+
+    list.push(now);
+
+    const recent = list.filter(t => now - t < config.timeWindow);
+
+    joins.set(guildId, recent);
+
+    if (recent.length >= config.joinLimit && !raid.get(guildId)) {
+      raid.set(guildId, true);
+
+      const channel = member.guild.systemChannel;
+      if (channel) channel.send("🚨 Possível raid detectado!");
+
+      setTimeout(() => {
+        raid.set(guildId, false);
+      }, 30000);
+    }
+
+    if (raid.get(guildId)) {
+      try {
+        await member.kick("Anti-raid ativo");
+      } catch {}
+    }
+  });
+};
