404aab2da52009fdb7719a3fc88ebd4896a2c9e3
silvazzy51-crypto <silvazzy51@gmail.com>
2026-05-18 16:03:54 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,1 @@
-console.log("BOT INICIANDO...");
 
-const { Client, GatewayIntentBits } = require("discord.js");
-
-const client = new Client({
-  intents: [GatewayIntentBits.Guilds]
-});
-
-client.once("ready", () => {
-  console.log("BOT ONLINE:", client.user.tag);
-});
-
-client.login(process.env.TOKEN);
